778f89c903ed50a5124ef1e2fddbfb51855c1b8f
grc <grc@gmail.com>
2023-05-26 16:11:42 -0600

    Microservicio de consulta de productos

---
+++ entities/product.go
@@ -0,0 +1,8 @@
+package entities
+
+type Product struct {
+	ID          uint    `json:"id"`
+	Name        string  `json:"name"`
+	Price       uint    `json:"account_non_expire"`
+	Description uint    `json:"active"`
+}
